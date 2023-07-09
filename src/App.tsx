9ea5bab29ff07a3e8942c80bf9d36af538b479c1
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-09 18:33:34 -0300

    feat: Add default theme and styled components types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+
 import { BrowserRouter } from 'react-router-dom'
+
+import { defaultTheme } from './styles/themes/default'
 
 import { Router } from './Router'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
