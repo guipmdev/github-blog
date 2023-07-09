87fad84ff189fc3cf088f7f3f11d08b61701cce7
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-09 18:22:07 -0300

    feat: Add routing functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './Router'
+
 export function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
