d1062636000cb2f75c1036c29cf958e7cca79e21
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-09 19:20:57 -0300

    style: Improve header layout and add cover image

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+import coverImage from '../../assets/cover.svg'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: center / cover no-repeat url(${coverImage});
+  height: 296px;
+
+  img {
+    width: 148px;
+  }
+`
