677d28847e605a2f61bee40cf2b2aaf2ea2f3694
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-09 21:04:11 -0300

    feat(UserCard): add UserCard component to Home page

---
+++ src/pages/Home/components/UserCard/index.tsx
@@ -0,0 +1,49 @@
+import { UserCardContainer, UserInfo } from './styles'
+
+import {
+  FaGithub,
+  FaBuilding,
+  FaUserGroup,
+  FaArrowUpRightFromSquare,
+} from 'react-icons/fa6'
+
+export function UserCard() {
+  return (
+    <UserCardContainer>
+      <img src="https://github.com/diego3g.png" alt="" />
+
+      <UserInfo>
+        <div>
+          <h2>Cameron Williamson</h2>
+          <p>
+            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+            volutpat pulvinar vel mass.
+          </p>
+        </div>
+
+        <div className="links">
+          <span>
+            <FaGithub /> diego3g
+          </span>
+
+          <span>
+            <FaBuilding /> Rocketseat
+          </span>
+
+          <span>
+            <FaUserGroup /> 32 seguidores
+          </span>
+        </div>
+      </UserInfo>
+
+      <a
+        href="https://github.com/diego3g"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GITHUB <FaArrowUpRightFromSquare />
+      </a>
+    </UserCardContainer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,58 +1,11 @@
-import {
-  HomeContainer,
-  UserCard,
-  UserInfo,
-  BlogFeed,
-  SearchForm,
-  Articles,
-} from './styles'
+import { HomeContainer, BlogFeed, SearchForm, Articles } from './styles'
 
-import {
-  FaGithub,
-  FaBuilding,
-  FaUserGroup,
-  FaArrowUpRightFromSquare,
-} from 'react-icons/fa6'
+import { UserCard } from './components/UserCard'
 
 export function Home() {
   return (
     <HomeContainer>
-      <UserCard>
-        <img src="https://github.com/diego3g.png" alt="" />
-
-        <UserInfo>
-          <div>
-            <h2>Cameron Williamson</h2>
-            <p>
-              Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-              viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-              volutpat pulvinar vel mass.
-            </p>
-          </div>
-
-          <div className="links">
-            <span>
-              <FaGithub /> diego3g
-            </span>
-
-            <span>
-              <FaBuilding /> Rocketseat
-            </span>
-
-            <span>
-              <FaUserGroup /> 32 seguidores
-            </span>
-          </div>
-        </UserInfo>
-
-        <a
-          href="https://github.com/diego3g"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GITHUB <FaArrowUpRightFromSquare />
-        </a>
-      </UserCard>
+      <UserCard />
 
       <BlogFeed>
         <SearchForm />
