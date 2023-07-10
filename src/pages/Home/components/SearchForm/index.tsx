280b980552a5f13025b65a8353fdf12a364ca0c0
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-10 20:20:24 -0300

    feat(SearchForm): add search functionality with form validation

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,14 +1,47 @@
 import { SearchFormContainer } from './styles'
 
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useForm } from 'react-hook-form'
+
+import { useEffect } from 'react'
+
+const searchPostsFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchPostsFormInputs = z.infer<typeof searchPostsFormSchema>
+
 export function SearchForm() {
+  const { register, watch, handleSubmit } = useForm<SearchPostsFormInputs>({
+    resolver: zodResolver(searchPostsFormSchema),
+  })
+
+  const watchQuery = watch('query')
+
+  function handleSearchPosts(data: SearchPostsFormInputs) {
+    console.log({ data })
+  }
+
+  useEffect(() => {
+    const submitTimeout = setTimeout(() => {
+      handleSubmit(handleSearchPosts)()
+    }, 2000)
+
+    return () => {
+      clearTimeout(submitTimeout)
+    }
+  }, [handleSubmit, watchQuery])
+
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <div>
         <h2>Publicações</h2>
         <span>6 publicações</span>
       </div>
 
-      <input type="text" placeholder="Buscar conteudo" />
+      <input type="text" placeholder="Buscar conteudo" {...register('query')} />
     </SearchFormContainer>
   )
 }
