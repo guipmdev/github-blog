c013ac3ecf84a876d63f97d4b8252dc947faa438
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-11 14:35:43 -0300

    style: update loading spinner to PropagateLoader

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,16 +2,17 @@ import { LoadingContainer } from './styles'
 
 import { useTheme } from 'styled-components'
 
-import { BounceLoader } from 'react-spinners'
+import { PropagateLoader } from 'react-spinners'
 
 export function Loading() {
   const theme = useTheme()
 
   return (
     <LoadingContainer>
-      <BounceLoader
+      <PropagateLoader
         color={theme['base-label']}
         size={75}
+        speedMultiplier={0.75}
         aria-label="Loading Spinner"
         data-testid="loader"
       />
